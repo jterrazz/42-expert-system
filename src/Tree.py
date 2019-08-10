736028bc67a39167c4d00d37df19d1ff06cd5b6a
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-08-10 19:06:55 +0200

    Noice

--- src/Tree.py
+++ src/Tree.py
@@ -2,46 +2,10 @@
 from Node import AtomNode, ConnectorNode, ConnectorType, Sign
 
 
-# TODO Test to print with no nodes
 class Tree:
     def __init__(self):
         self.atoms = []
-        rootNode = ConnectorNode(ConnectorType.AND)
-
-        atom_a = AtomNode('A')
-        atomB = AtomNode('B')
-        atomC = AtomNode('C')
-        atomD = AtomNode('D')
-        atomE = AtomNode('E')
-
-        rootNode.append_operand(atom_a, Sign.POSITIVE)
-        rootNode.append_operand(atomB, Sign.POSITIVE)
-        rootNode.append_operand(atomC, Sign.POSITIVE)
-        rootNode.append_operand(atomD, Sign.POSITIVE)
-        rootNode.append_operand(atomE, Sign.POSITIVE)
-        self.root_node = rootNode
-
-        connectorBC = ConnectorNode(ConnectorType.AND)
-        connectorBC.append_operand(atomB, Sign.NEGATIVE)
-        connectorBC.append_operand(atomC, Sign.POSITIVE)
-
-        atom_a.append_child(connectorBC, Sign.POSITIVE)
-
-        connectorDE = ConnectorNode(ConnectorType.OR)
-        connectorDE.append_operand(atomD, Sign.POSITIVE)
-        connectorDE.append_operand(atomE, Sign.POSITIVE)
-        atomB.append_child(connectorDE, Sign.POSITIVE)
-
-        atomC.append_child(atomB, Sign.POSITIVE)
-
-        connectorBC2 = ConnectorNode(ConnectorType.AND)
-        connectorBC2.append_operand(atomC, Sign.POSITIVE)
-        connectorBC2.append_operand(atomB, Sign.POSITIVE)
-
-        connectorBCorA = ConnectorNode(ConnectorType.OR)
-        connectorBCorA.append_operand(connectorBC2, Sign.POSITIVE)
-        connectorBCorA.append_operand(atom_a, Sign.POSITIVE)
-        atomD.append_child(connectorBCorA, Sign.POSITIVE)
+        self.root_node = ConnectorNode(ConnectorType.AND)
 
     def add_atom(self, node):
         self.root_node.append_operand(node, Sign.POSITIVE)
@@ -53,8 +17,8 @@ class Tree:
     @staticmethod
     def print_node_handler(node, negative, level):
         str = ' ' * level * 4
-        if (negative):
-            str += '\033[91m-\033[0m'
+        if negative:
+            str += '\033[97m!\033[0m'
         if isinstance(node, ConnectorNode):
             return str + '\033[95m' + node.__repr__() + '\033[0m\n'
         return str + '\033[94m' + node.__repr__() + '\033[0m\n'
@@ -68,5 +32,45 @@ class Tree:
             str += res
         return str
 
+
+
+# Example
+
 tree = Tree()
+
+atom_a = AtomNode('A')
+atomB = AtomNode('B')
+atomC = AtomNode('C')
+atomD = AtomNode('D')
+atomE = AtomNode('E')
+
+tree.add_atom(atom_a)
+tree.add_atom(atomB)
+tree.add_atom(atomC)
+tree.add_atom(atomD)
+tree.add_atom(atomE)
+
+connectorBC = ConnectorNode(ConnectorType.AND)
+connectorBC.append_operand(atomB, Sign.POSITIVE)
+connectorBC.append_operand(atomB, Sign.NEGATIVE)  # When the algo will check for incoherence it will bug here
+connectorBC.append_operand(atomC, Sign.POSITIVE)
+
+atom_a.append_child(connectorBC, Sign.POSITIVE)
+
+connectorDE = ConnectorNode(ConnectorType.OR)
+connectorDE.append_operand(atomD, Sign.POSITIVE)
+connectorDE.append_operand(atomE, Sign.POSITIVE)
+atomB.append_child(connectorDE, Sign.POSITIVE)
+
+atomC.append_child(atomB, Sign.POSITIVE)
+
+connectorBC2 = ConnectorNode(ConnectorType.AND)
+connectorBC2.append_operand(atomC, Sign.POSITIVE)
+connectorBC2.append_operand(atomB, Sign.POSITIVE)
+
+connectorBCorA = ConnectorNode(ConnectorType.OR)
+connectorBCorA.append_operand(connectorBC2, Sign.POSITIVE)
+connectorBCorA.append_operand(atom_a, Sign.POSITIVE)
+atomD.append_child(connectorBCorA, Sign.POSITIVE)
+
 print(tree)

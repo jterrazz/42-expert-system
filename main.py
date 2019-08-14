46977020a3e7468ec55a1f81ff1489e939eb9f3e
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-08-14 07:41:39 +0200

    clean

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import re
 import sys
 
-from expert_system.helpers.Color import Ft_colors
 from expert_system.parsers.ExpertParser import ExpertParser
 
-# tmp = '! ( A + B ) ^ ( D | K )'
-# tmp = re.split('\s', tmp)
-# infix_to_postfix(tmp)
 
-
-# Main Function
 if __name__ == "__main__":
     try:
         with open(sys.argv[1]) as f:
@@ -17,12 +10,5 @@ if __name__ == "__main__":
     except:
         print('Error opening file for reading ..!!')
 
-    # TMP
-    # content = "A + B => C"
     parser = ExpertParser(content)
 
-    # bcolors = Ft_colors()
-    # if ft_parser(content):
-    #     print(bcolors.OKGREEN + "OK" + bcolors.ENDC)
-    # else:
-    #     print(bcolors.FAIL + "KO" + bcolors.ENDC)

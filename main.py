81f21599b4615603735b52c4c4bd163711a6c34a
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-08-15 18:20:00 +0200

    Save before refactor

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ if __name__ == "__main__":
     try:
         with open(sys.argv[1]) as f:
             content = f.readlines(1000) # TODO anyway to set no limits ???
-            parser = ExpertParser(content)
-
-            tree = NPITree(parser.structured_rules, parser.facts, parser.queries)
-
-            for query in parser.queries:
-                print(f"Resolve {query}", tree.resolve_atom(query))
     except: # TODO Except only for Reading errors
         print('Error opening file for reading ..!!')
+    finally:
+        parser = ExpertParser(content)
+
+        tree = NPITree(parser.structured_rules, parser.facts, parser.queries)
+
+        for query in parser.queries:
+            print(f"Resolve {query}", tree.resolve_atom(query))

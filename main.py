81fc469cba4482b89236a5dc1cccad1e4905e9a9
abbensid@gmail.com <abbensid@gmail.com>
2019-08-16 14:37:18 +0200

    check type of file, add function tests

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from expert_system.Tree import NPITree
 if __name__ == "__main__":
     try:
         with open(sys.argv[1]) as f:
-            content = f.readlines()  # TODO Do we really need the 1000 limit ???
-    except:
-        print('Error opening file for reading')
-        raise
+            content = f.readlines(1000)
+    except Exception as e:
+        print("{}".format(e))
+        sys.exit(1)
 
     parser = ExpertParser(content)
     tree = NPITree(parser.structured_rules, parser.facts, parser.queries)

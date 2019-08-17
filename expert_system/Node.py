e1c94f695ac21ea602141eecf29d1e9c7511df5b
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-08-17 10:52:50 +0200

    negative

--- expert_system/Node.py
+++ expert_system/Node.py
@@ -1,5 +1,4 @@
 from enum import Enum
-import itertools
 
 
 class ConnectorType(Enum):
@@ -9,88 +8,25 @@ class ConnectorType(Enum):
     IMPLY = '<='
 
 
-# # Use common structure class with children and parents
-# class NegativeNode:
-#     def __init__(self, positive_node):
-#         self.node = positive_node
-#         self.children = []
-#         self.parents = []
-#
-#     def add_child(self, child):
-#         if child not in self.children:
-#             self.children.append(child)
-#         if self not in child.parents:
-#             child.parents.append(self)
-#
-#     def parse_handler(self, node_handler, result_handler, negative, level, force_node):
-#         if not self.children.__len__():
-#             return ' ' * level + f'-{ self.node.__repr__()}\n'
-#
-#         str = ' ' * level + "-\n"
-#         for child in self.children:
-#             str += child.parse_handler(node_handler, result_handler, True, level + 4, False)
-#         str += self.node.parse_handler(node_handler, result_handler, False, level, False)
-#         return str
-#         # return self.parse_handler(node_handler, result_handler, True, level, force_node)
+class NegativeNode:
+    def __init__(self, child, parent):
+        # TODO Add a only one must be set assert
+        self.child = child
+        # TODO Example: X =>  !A + B
+        self.operand_parent = parent
+
+    def __repr__(self):
+        return f"-{ self.child }"
+
+    def solve(self):
+        r = self.child.solve()
+        return not r if r is not None else None
+
+    def set_value(self, value):
+        v = not value if value is not None else None
+        self.child.set_value(v)
 
 
-# class OperandState:
-#     def __init__(self, name, state):
-#         self.state = state
-#         self.name = name
-#
-#     def __eq__(self, other):
-#         return self.name == other.name
-#
-#     def __repr__(self):
-#         return f"name: { self.name }, state: { self.state }"
-#
-#
-# # Need to keep in memory the replaced subsets (lets say we have A + B True and B + C True)
-# class ConnectorSimplifier:
-#     def __init__(self, type, operands):
-#         self.type = type
-#         self.operands = [OperandState(op.name, op.state) for op in operands]
-#
-#     def replace(self, operands, value):
-#         name = ""
-#         # If value not found raise
-#         for op in operands:
-#             name += op.name
-#             self.operands.remove(op)
-#         print("Will simplify the subset", name, " with value", value)
-#         self.operands.append(OperandState(name, value))
-#
-#     def get_result(self):
-#         found_none = False
-#         res = None
-#
-#         for op in self.operands:
-#             if op.state is None:
-#                 found_none = True
-#                 continue
-#             elif res is None:
-#                 res = op.state
-#                 continue
-#             elif self.type is ConnectorType.AND:
-#                 res &= op.state
-#             elif self.type is ConnectorType.OR:
-#                 res |= op.state
-#             elif self.type is ConnectorType.XOR:
-#                 res ^= op.state
-#
-#         if found_none:
-#             if (self.type is ConnectorType.OR and res is False) or (self.type is ConnectorType.AND and res is True) or (self.type is ConnectorType.XOR):
-#                 return None
-#         print("Simplifier returns", res)
-#         return res
-
-# A => E
-# X => A + C
-
-
-# Rename parent to operand_parent (find the Real name maybe connector)
-# Find example of a Connector that must be resolved thanks to its parent
 class Node:
     """
     A Node is the main element stored in the tree. Each node is connected to each other in a parent/child relation.
@@ -106,7 +42,6 @@ class Node:
         self.visited = False
         self.state = False
         self.tree = tree
-        # self.negative = NegativeNode(self)
 
     def __repr_color__(self, str):
         if self.state is True:

8f0ffefa75d9b2fc8548d3bdb110bc501e854041
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-08-17 22:55:09 +0200

    Good tests start

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,25 +2,38 @@ import pytest
 from os import listdir
 from os.path import isfile, join
 from main import resolve_lines
+from expert_system.parser.Parser import ExpertParser
+from expert_system.Tree import NPITree
 
 def get_all_files(path):
     return [f for f in listdir(path) if isfile(join(path, f))]
 
-# results = [
-#     ()
-# ]
-#
-# @pytest.mark.parametrize('input, expected', [(1, 0)])
-# def test_bad_files(input, expected):
+# Without the folder path
+good_results = [
+    ("and.txt", [True, False])
+]
+
+@pytest.mark.parametrize('input, expected', good_results)
+def test_good_files(input, expected):
+    with open("./tests/_examples/good_files/" + input) as f:
+        file_lines = f.readlines(1000)
+    parser = ExpertParser(file_lines)
+    tree = NPITree(parser.structured_rules, parser.facts, parser.queries)
+    i = 0
+    for query in parser.queries:
+        assert expected[i] == tree.resolve_query(query)
+        i += 1
 
 
 bad_files_path = "./tests/_examples/bad_files"
 bad_files = [bad_files_path + f for f in get_all_files(bad_files_path)]
+
+
 @pytest.mark.parametrize('input', bad_files)
 def test_bad_files(input):
     try:
-        with open(input) as f:  # TODO protect argv
-            file_lines = f.readlines(1000)  # TODO Maybe we should actually do more than 1000
+        with open(input) as f:
+            file_lines = f.readlines(1000)
         resolve_lines(file_lines)
     except:
         pass

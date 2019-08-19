b0b26efaf43102d72356c819bc82b4b82fd51d4c
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-08-19 15:22:20 +0200

    Yo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import argparse
 
-# from expert_system.Prompt import ExpertPrompt
+from expert_system.Prompt import ExpertPrompt
 from expert_system.parser.Parser import ExpertParser
 from expert_system.Tree import NPITree
 from expert_system.ShowTree import ShowTree
@@ -9,18 +9,9 @@ from expert_system.ShowTree import ShowTree
 args = None
 
 
-def resolve_lines(lines):
-    parser = ExpertParser(lines)
-
-    if args and args.d:
-        ShowTree(parser.structured_rules, parser.facts, parser.queries).display_tree_in_shell()
-    if args and args.r:
-        ShowTree(parser.structured_rules, parser.facts, parser.queries).display_rules()
-    # ExpertPrompt().cmdloop()
-
+def resolve_lines(parser):
     # create tree image
     # ShowTree(parser.structured_rules, parser.facts, parser.queries).create_image()
-
     tree = NPITree(parser.structured_rules, parser.facts, parser.queries)
     for query in parser.queries:
         print(f"Resolve {query}", tree.resolve_query(query))
@@ -29,7 +20,7 @@ def resolve_lines(lines):
 if __name__ == "__main__":
 
     flag = argparse.ArgumentParser(description='ExpertSystem @ Paris 42 School - Made by @abbensid and @jterrazz')
-    flag.add_argument("-m", choices=['mode_shell', 'mode_interactive'], default='mode_shell', help="Interface mode")
+    flag.add_argument("-m", choices=['shell', 'interactive'], default='mode_shell', help="Interface mode")
     flag.add_argument("-d", action='store_true', help="Displays the graph")
     flag.add_argument("-r", action='store_true', help="Displays the rules")
     flag.add_argument("-i", action='store_true', help="create graph system")
@@ -39,8 +30,18 @@ if __name__ == "__main__":
 
     try:
         with open(args.input) as f:
-            file_lines = f.readlines() # TODO Maybe we should actually do more than 1000
-        resolve_lines(file_lines)
+            lines = f.readlines() # TODO Maybe we should actually do more than 1000
+
+        parser = ExpertParser(lines)
+
+        if args.m == "interactive":
+            ExpertPrompt().cmdloop()
+        else:
+            if args and args.d:
+                ShowTree(parser.structured_rules, parser.facts, parser.queries).display_tree_in_shell()
+            if args and args.r:
+                ShowTree(parser.structured_rules, parser.facts, parser.queries).display_rules()
+            resolve_lines(parser)
 
     except (Exception, BaseException) as e:
         print("{}".format(e))

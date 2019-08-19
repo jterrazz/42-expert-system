2cb3679c7eefab83a4034f5426cbc6c8fc2f7787
abbensid@gmail.com <abbensid@gmail.com>
2019-08-19 09:46:53 +0200

    change name of node in ShowTree

--- expert_system/ShowTree.py
+++ expert_system/ShowTree.py
@@ -5,28 +5,30 @@ from .parser.Rule import OPERATORS, ImplicationType
 LST_Implication = {ImplicationType.EQUAL :'<=>', ImplicationType.IMPLY :'=>'}
 
 class ShowTree:
+    count = 0
     def __init__(self, rules):
         self.rules = rules
 
-    def create_part_tree(self, NPI_part, part, Implication, rule_index):
+    def create_part_tree(self, NPI_part, Implication):
         stack = []
         i = 0
         for x in NPI_part:
             if x == '!':
                 i += 1
                 pop = stack.pop()
-                connector_not = Node(x + 'not' + part + str(i) + str(rule_index), display_name=x)
+                connector_not = Node(str(ShowTree.count), display_name=x)
                 pop.parent = connector_not
                 stack.append(connector_not)
             elif x in OPERATORS:
                 i += 1
                 pop0 = stack.pop()
                 pop1 = stack.pop()
-                connector = Node(x + part + str(i) + str(rule_index), display_name=x)
+                connector = Node(str(ShowTree.count), display_name=x)
                 pop0.parent, pop1.parent = connector, connector
                 stack.append(connector)
             else:
                 stack.append(Node(x, display_name=x))
+            ShowTree.count += 1
         pop = stack.pop()
         pop.parent = Implication
 
@@ -34,9 +36,9 @@ class ShowTree:
         root = Node('root', display_name='root')
         for key, val in enumerate(self.rules):
             imp = Node(LST_Implication[val.type] + str(key), parent=root, display_name=LST_Implication[val.type])
-            self.create_part_tree(val.npi_left, 'left', imp, key)
-            self.create_part_tree(val.npi_right, 'right', imp, key)
+            self.create_part_tree(val.npi_left, imp)
+            self.create_part_tree(val.npi_right, imp)
 
         # create image
         DotExporter(root,
-                nodeattrfunc=lambda node: 'label="{}"'.format(node.display_name)).to_picture("graph.png")
+            nodeattrfunc=lambda node: 'label="{}"'.format(node.display_name)).to_picture("graph.png")

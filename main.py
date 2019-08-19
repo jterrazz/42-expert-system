84f0a932b646473fde9547ca51d8baa3ce31f44d
abbensid@gmail.com <abbensid@gmail.com>
2019-08-19 15:25:41 +0200

    add function create_array_rules_facts_queries, for history

--- main.py
+++ main.py
@@ -21,15 +21,15 @@ def resolve_lines(lines):
 if __name__ == "__main__":
 
     # Parser
-    # flag = argparse.ArgumentParser(description='ExpertSystem @ 42')
-    # flag.add_argument("-m", choices=['mode_shell', 'mode_interactive'], default='mode_shell', help="show mode")
-    # flag.add_argument("-d", action='store_true', help="display graph system")
-    # flag.add_argument("-r", action='store_true', help="display rules system")
-    # flag.add_argument("-i", action='store_true', help="create graph system")
-    # flag.add_argument("-hist", action='store_true', help="display historique system")
-    # flag.add_argument("input", help="input system")
-    # flag.parse_args()
-    # args = flag.parse_args()
+    flag = argparse.ArgumentParser(description='ExpertSystem @ 42')
+    flag.add_argument("-m", choices=['mode_shell', 'mode_interactive'], default='mode_shell', help="show mode")
+    flag.add_argument("-d", action='store_true', help="display graph system")
+    flag.add_argument("-r", action='store_true', help="display rules system")
+    flag.add_argument("-i", action='store_true', help="create graph system")
+    flag.add_argument("-hist", action='store_true', help="display historique system")
+    flag.add_argument("input", help="input system")
+    flag.parse_args()
+    args = flag.parse_args()
 
     try:
         with open(sys.argv[1]) as f: # TODO protect argv
@@ -40,21 +40,21 @@ if __name__ == "__main__":
         print("{}".format(e))
         sys.exit(1)
 
-    # # flag mode
-    # if args.m == 'mode_shell':
-    #     print('mode Shell')
-    # elif args.m == 'mode_interactive':
-    #     print('mode interactive')
-    # else:
-    #     print('mode Shell')
-    #
-    # parser = ExpertParser(file_lines)
-    # # flag display graph
-    # if args.d:
-    #     ShowTree(parser.structured_rules, parser.facts, parser.queries).display_tree_in_shell()
-    #
-    # # flag display rules
-    # if args.r:
-    #     ShowTree(parser.structured_rules, parser.facts, parser.queries).display_rules()
+    # flag mode
+    if args.m == 'mode_shell':
+        print('mode Shell')
+    elif args.m == 'mode_interactive':
+        print('mode interactive')
+    else:
+        print('mode Shell')
+
+    parser = ExpertParser(file_lines)
+    # flag display graph
+    if args.d:
+        ShowTree(parser.structured_rules, parser.facts, parser.queries).display_tree_in_shell()
+
+    # flag display rules
+    if args.r:
+        ShowTree(parser.structured_rules, parser.facts, parser.queries).display_rules()
 
     # ExpertSystem().cmdloop()
